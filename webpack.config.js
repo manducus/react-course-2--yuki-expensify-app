c1920baacf2f569f93462fe46f45395f536da60e
Yuki Uehara <yukiu496@gmail.com>
2020-03-28 11:39:42 +0900

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,58 @@
 const path = require("path")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: ["./src/app.js"],
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        },{
-            test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-            // whenever webpack encounters a .scss file,
-            // first: it gets the .scss code
-            // second: convert .scss into .css
-            // finally; get it showing up in the browser by dumping it into a style tag
-        }]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-        // tells devServer that we're going to be handling routing via client side code,
-        // and that it should return index.html for all 404 routes
-    },
-    devtool: "cheap-module-eval-source-map"
+module.exports = (env) => {
+    const isProduction = env === "production"
+
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: ["./src/app.js"],
+        output: {
+            path: path.resolve(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },{
+                test: /\.s?css$/,
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                    // style-loader handles the inlining of the styles
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+                // whenever webpack encounters a .scss file,
+                // first: it gets the .scss code
+                // second: convert .scss into .css
+                // finally; get it showing up in the browser by dumping it into a style tag
+            }]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+            // tells devServer that we're going to be handling routing via client side code,
+            // and that it should return index.html for all 404 routes
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "styles.css"
+            })
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map"
+    }
 }
 
 // node-sass takes our Sass or SCSS code & converts it into regular CSS
